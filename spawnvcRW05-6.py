4dbc7a27f04d8f42c36b6d386a50483f82bf040b
kablammy7 <126311188+kablammy7@users.noreply.github.com>
2023-03-05 20:00:40 -0500

    Update and rename spawnvcRW05-4.py to spawnvcRW05-6.py

--- spawnvcRW05-6.py
+++ spawnvcRW05-6.py
@@ -1,29 +1,47 @@
 
-#spawnvcPC05-4.py
+#spawnvcPC05-6.py
 
 import os
+import threading
 import asyncio
 from datetime import datetime, timedelta
 import time
 import discord
-from discord.ext import commands
+#from discord.ext import commands
 from discord.ext import commands, tasks
 import re
 #from reportlab.pdfgen import canvas
+
+
 
 # uncomment the 2 lines below for PC deploy
 # comment the 2 lines below for railway deploy
 #from dotenv import load_dotenv
 #load_dotenv('spammytest.env')
 
-intents = discord.Intents().all()
+#intents = discord.Intents().all()
 #intents = discord.Intents().default()
-#intents = discord.Intents(members=True, voice_states=True, value=True)
-#client = discord.Client(intents=intents)
+intents = discord.Intents(members=True, voice_states=True, value=True)
+client = discord.Client(intents=intents)
 client = commands.Bot(command_prefix='?', intents=intents)
 
-ZuluDiff = -5
+
+
+
+
+
+
+
+
+
+
+
+zuluDiff = -5
 patternGetInt = r"^\D\D(\d{2})"
+channelsData = {}
+lockReporting = False
+reportNumber = 0
+#guildData = {}
 
 
 
@@ -37,7 +55,46 @@ patternGetInt = r"^\D\D(\d{2})"
 
 
 
-client.command()
+
+
+
+
+
+#async def my_function():
+#    # Do something here
+#    print("My function ran!")
+
+## Schedule the function to run every 10 minutes
+#async def run_function():
+#    while True:
+#        await my_function()
+#        await asyncio.sleep(10)  
+
+
+
+
+
+#async def my_coroutine():
+#    while True:
+#        print("Running my coroutine!")
+#        await asyncio.sleep(10)
+
+#def start_loop(loop):
+#    asyncio.set_event_loop(loop)
+#    loop.run_forever()
+
+
+#loop = asyncio.new_event_loop()
+#thread = threading.Thread(target=start_loop, args=(loop,))
+#thread.start()
+
+#asyncio.run(my_coroutine())
+
+
+
+
+
+
 async def restart(ctx):
 
     message = client.guilds[0].name + ' server is restarting'
@@ -107,7 +164,8 @@ async def latency(ctx):
 
 @client.event
 async def on_ready():
-
+    lockRorting = False
+    reportNumber = 0
     print ('\n\rLogged in as {0.user}'.format(client))
     print(f'Connected to {len(client.guilds)} guilds')
 
@@ -120,32 +178,21 @@ async def on_ready():
         adjustmentsMade = False
         channels = guild.voice_channels
         
+        # sort the voice channels
+        target_channel = discord.utils.get(guild.voice_channels, name="MakeNewChannel")
+        all_channels = guild.voice_channels
+        target_index = all_channels.index(target_channel)
+        vc_channels = sorted([c for c in all_channels if c.name.startswith("VC")], key=lambda c: c.name)
+        for i, c in enumerate(vc_channels):
+            await c.edit(position=target_index+i+1)
+
         # Remove empty VC channels
         for channel in channels:
             #print ('start channel name = ' + channel.name)
-            if (channel.name.startswith('VC') and len(channel.members) == 0):
+            if ((channel.name.startswith('VC')) and (len(channel.members) == 0)):
                 await channel.delete()
                 adjustmentsMade = True
                 print (f"Guild [{guild.name}] deleted [{channel.name}]")
-                # Get the target channel to place the sorted channels after
-                target_channel = discord.utils.get(guild.voice_channels, name="MakeNewChannel")
-
-                # Get all voice channels in the guild
-                all_channels = guild.voice_channels
-
-                # Get the index of the target channel in the list of all channels
-                target_index = all_channels.index(target_channel)
-
-                # Get the "VC" channels and sort them alphabetically
-                vc_channels = sorted([c for c in all_channels if c.name.startswith("VC")], key=lambda c: c.name)
-
-                # Update the channel positions in the guild to place the "VC" channels after the target channel
-                for i, c in enumerate(vc_channels):
-                    await c.edit(position=target_index+i+1)
-
-#                # Print the sorted voice channels (optional)
-#                for c in all_channels:
-#                    print(c.name)
 
         # move members out of MakeNewChannel channel   
         for channel in channels:
@@ -154,18 +201,20 @@ async def on_ready():
                 lenChannelMembers = len(channel.members)
                 while len(channel.members) > 0:
                     mncMember =  channel.members[0]
-
-
                     existingChannels = [c for c in channel.guild.voice_channels if c.name.startswith("VC")]
-                
                     newChannelNumber = getNewChannelNumber(existingChannels)
-
                     newChannelName = f"VC{newChannelNumber:02} {mncMember.display_name.split('#')[0]}"
-
                     movedToChannel = await channel.category.create_voice_channel(newChannelName)
                     await mncMember.move_to(movedToChannel)
                     adjustmentsMade = True
                     print (f"Guild [{guild.name}] channel [{newChannelName}] created moved [{mncMember.name}]")
+                    # sort the voice channels
+                    target_channel = discord.utils.get(guild.voice_channels, name="MakeNewChannel")
+                    all_channels = guild.voice_channels
+                    target_index = all_channels.index(target_channel)
+                    vc_channels = sorted([c for c in all_channels if c.name.startswith("VC")], key=lambda c: c.name)
+                    for i, c in enumerate(vc_channels):
+                        await c.edit(position=target_index+i+1)
 
         message = ('adjustments made = 'f"{adjustmentsMade} on {guild.name} server")
         print ('\n\rfinished cleaning up ' + message)
@@ -174,13 +223,13 @@ async def on_ready():
         print ('sending message')
         await member.send(message)
 
+    
 
 
-
-
-
-
-        
+    botGuilds = client.guilds
+    print ('guilds set')
+    report.start()    # timer task for future use
+    
     print ('exiting on_ready now')
     exit
 
@@ -254,6 +303,73 @@ def getNewChannelNumber (existingChannels):
 
 
 
+@tasks.loop(seconds = 10) # repeat in 1 minute
+async def report():
+    
+    global reportNumber
+
+    if not lockReporting:
+        print ('report starting ' + str(reportNumber))
+        reportNumber += 1
+        for guildName, channels in channelsData.items():
+            print(guildName)
+            for channelName, members in channels.items():
+                print(channelName, ' '.join(members), sep=' ')
+            print()  # Print a new line between guilds
+    channelsData.clear()
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -267,27 +383,25 @@ def getNewChannelNumber (existingChannels):
 @client.event
 async def on_voice_state_update(member, before, after):
 
-    
-
-#     print(f"\n\r01 --> {datetime.now() + timedelta(hours=ZuluDiff)} activity detected {member.display_name.split('#')[0]} (member name) {member.name}")
-##    print (f"{before.channel}    VC {member.display_name.split('#')[0]}")
-##    print (f"{after.channel}       MakeNewChannel")
-##    
-##    print (f"{before.channel}" == f"VC {member.display_name.split('#')[0]}")
-##    print (f"{after.channel}" != "MakeNewChannel")
-##
-##    if (f"{before.channel}" == f"VC {member.display_name.split('#')[0]}") and \
-##    (f"{after.channel}" != "MakeNewChannel"):
-        
-#     print (f"\n\r01 --> {datetime.now() + timedelta(hours=ZuluDiff)} activity detected {member.display_name.split('#')[0]} (member name) {member.name}")
-
-
     guild = member.guild
+    nlcr = '\n\r'
 
     memberDisplayName = member.display_name.split('#')[0]
     memberName = member.name
+    #if after.channel is not None and before.channel is None:
+    
+    if (before.channel is None):
+        beforeChannel = 'None'
+    else: beforeChannel=(f"{before.channel}")
+    if (after.channel is None):
+        afterChannel = 'None'
+    else: afterChannel=(f"{after.channel}")
 
-    if after.channel is not None and after.channel.name == "MakeNewChannel":
+    if beforeChannel == "MakeNewChannel": nlcr = ''
+    print (f"{nlcr}01 --> {truncate_datetime(datetime.now() + timedelta(hours=zuluDiff))} Guild [{guild.name}] [{memberDisplayName}] - [{memberName}] left channel [{beforeChannel}] joined channel {afterChannel}")
+
+    # join MakeNewChannel
+    if (after.channel is not None) and (after.channel.name == "MakeNewChannel"):
         category = after.channel.category
         existingChannels = [c for c in after.channel.guild.voice_channels if c.name.startswith("VC")]
         newChannelNumber = getNewChannelNumber(existingChannels)
@@ -295,41 +409,79 @@ async def on_voice_state_update(member, before, after):
 
         channel = await category.create_voice_channel(newChannelName)
         await member.move_to(channel)
-        print (f"01 --> {truncate_datetime(datetime.now() + timedelta(hours=ZuluDiff))} Guild [{guild.name}] [{memberDisplayName}] - [{memberName}] move to new channel [{channel}]")
- 
-    if before.channel is not None and 'VC' in str({before.channel}):
+        # sort the voice channels
+        target_channel = discord.utils.get(guild.voice_channels, name="MakeNewChannel")
+        all_channels = guild.voice_channels
+        target_index = all_channels.index(target_channel)
+        vc_channels = sorted([c for c in all_channels if c.name.startswith("VC")], key=lambda c: c.name)
+        for i, c in enumerate(vc_channels):
+            await c.edit(position=target_index+i+1)
+    
+    # channel vacated
+    if (before.channel is not None) and ('VC' in str({before.channel})):
         #memberName = f"VC[int(re.match(patternGetInt, channel.name).group(1))] {member.display_name.split('#')[0]}"
         if len(before.channel.members) == 0:
             await before.channel.delete()
-            print (f"02 --> {truncate_datetime(datetime.now() + timedelta(hours=ZuluDiff))} Guild [{guild.name}] [{memberDisplayName}] - [{memberName}] vacated channel [{before.channel}] deleted")
+            print (f"02 --> {truncate_datetime(datetime.now() + timedelta(hours=zuluDiff))} Guild [{guild.name}] [{memberDisplayName}] - [{memberName}] vacated channel [{before.channel}] deleted") #moved to {after.channel}")
 
-            # Get the target channel to place the sorted channels after
-            target_channel = discord.utils.get(guild.voice_channels, name="MakeNewChannel")
-
-            # Get all voice channels in the guild
-            all_channels = guild.voice_channels
-
-            # Get the index of the target channel in the list of all channels
-            target_index = all_channels.index(target_channel)
-
-            # Get the "VC" channels and sort them alphabetically
-            vc_channels = sorted([c for c in all_channels if c.name.startswith("VC")], key=lambda c: c.name)
-
-            # Update the channel positions in the guild to place the "VC" channels after the target channel
-            for i, c in enumerate(vc_channels):
-                await c.edit(position=target_index+i+1)
-
-#            # Print the sorted voice channels (optional)
-#            for c in all_channels:
-#                print(c.name)
-
+            
         else:
             beforeChannel = f"{before.channel}"
-            if((f"{before.channel}") != (f"{after.channel}")):
+            if((beforeChannel) != (f"{after.channel}")):
                 newName = f"VC{(re.match(patternGetInt, str(beforeChannel)).group(1))} {before.channel.members[0].display_name.split('#')[0]}"
-                await before.channel.edit(name=newName)
-                print (f"03 --> {truncate_datetime(datetime.now() + timedelta(hours=ZuluDiff))} Guild [{guild.name}] [{memberDisplayName}] - [{memberName}] before channel [{beforeChannel}] renamed to [{newName}]")
+                if (f"{before.channel}") != newName:
+                    await before.channel.edit(name=newName)
+                    print (f"03 --> {truncate_datetime(datetime.now() + timedelta(hours=zuluDiff))} Guild [{guild.name}] [{memberDisplayName}] - [{memberName}] before channel [{beforeChannel}] renamed to [{newName}]")
+                else: (f"04 --> {truncate_datetime(datetime.now() + timedelta(hours=zuluDiff))} Guild [{guild.name}] [{memberDisplayName}] - [{memberName}] before channel [{beforeChannel}] moved to [{after.channel}]")
  
+
+
+    lockReporting = True
+
+    for guild in client.guilds:
+        channelsData[guild.name] = {}
+        for channel in guild.voice_channels:
+            channelsData[guild.name][channel.name] = [member.name for member in channel.members]
+
+    lockReporting = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # PC deploy
 #client.run(os.getenv('TOKEN'))
@@ -338,3 +490,10 @@ async def on_voice_state_update(member, before, after):
 client.run(os.environ['TOKEN'])
 
 # https://discord.com/api/oauth2/authorize?client_id=1079357107771551814&permissions=16787472&scope=bot%20applications.commands
+
+
+
+
+
+
+
