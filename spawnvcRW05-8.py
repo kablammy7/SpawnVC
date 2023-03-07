58c9fb2e24b094db87aa63f85b0b53e1a452e7ae
kablammy7 <126311188+kablammy7@users.noreply.github.com>
2023-03-07 14:42:01 -0500

    Update spawnvcRW05-8.py

--- spawnvcRW05-8.py
+++ spawnvcRW05-8.py
@@ -291,7 +291,7 @@ async def report():
                 print(f"\n\rGuild: [{guild_name}]")
                 for channel_name, channel_members in guild_channels.items():
                     if channel_members:
-                        member_list = "\n\r".join(channel_members)
+                        member_list = "\n".join(channel_members)
                         channel_name += ' ->'
                         print(f"  {channel_name:<25}{member_list:>25}")
                         
