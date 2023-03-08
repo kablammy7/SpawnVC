faeaf7207cfe7bb85e9912c2a491fd466e5efc9c
kablammy7 <126311188+kablammy7@users.noreply.github.com>
2023-03-08 00:50:03 -0500

    Update spawnvcRW05-10.py

--- spawnvcRW05-10.py
+++ spawnvcRW05-10.py
@@ -481,10 +481,10 @@ async def on_voice_state_update(member, before, after):
 
 
 # PC deploy
-client.run(os.getenv('TOKEN'))  
+#client.run(os.getenv('TOKEN'))  
 
 #railway deploy 
-#client.run(os.environ['TOKEN'])
+client.run(os.environ['TOKEN'])
 
 # https://discord.com/api/oauth2/authorize?client_id=1079357107771551814&permissions=16787472&scope=bot%20applications.commands
 
